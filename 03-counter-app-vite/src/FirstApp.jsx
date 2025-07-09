acc123d38130d9edcc3c45c0570bcad6dedd86f2
Pvin <pvin@localhost.localdomain>
2025-07-09 12:32:19 -0600

    Seccion 3 => Primeros pasos en React => Finished

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+export const FirstApp = ({title, subtitle, numberUser}) => {
+    return (
+        <>
+            <h1>{title}</h1>
+            {/*<code> {JSON.stringify(newMessage)} </code>*/}
+            <p>{subtitle}</p>
+            <p>Your id is: {numberUser}</p>
+        </>
+    );
+};
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    numberUser: PropTypes.number.isRequired,
+};
+
+FirstApp.defaultProps = {
+    title: "No hay titulo! :c",
+    subtitle: "No hay subtitulo! :c",
+    name: "Kevin Carrillo"
+};
